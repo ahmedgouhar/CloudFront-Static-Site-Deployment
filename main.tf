ecb09ea3321f2ba6455544d2470856f56756244e
Ahmed Gouhar <waela8214g@mail.com>
2026-04-27 22:33:31 +0200

    upload architecture diagram

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "s3" {
-  source = "../../../ modules/s3"
+  source = "./modules/s3"
    cloudfront_arn = module.cloudfront.cdn_arn
   
 }
 
 
 module "cloudfront" {
-  source = "../../../ modules/cloudfront"
+  source = "./modules/cloudfront"
 
   s3_domain_name = module.s3.bucket_regional_domain_name
   # acm_certificate_arn = module.acm.certificate_arn
 }
 # module "acm" {
 #   domain_name = "cleanora.com"
-#   source = "../../../ modules/acm"
+#   source = "./modules/acm"
 #    zone_id     = module.dns.zone_id
 
 #   providers = {
@@ -21,7 +21,7 @@ module "cloudfront" {
 #   }
 # }
 # module "dns" {
-#   source = "../../../ modules/DNS"
+#   source = "./modules/DNS"
 #    cloudfront_domain    = module.cloudfront.domain_name_cloud
 #   cloudfront_zone_id   = "Z2FDTNDATAQYW2"
 #    domain_name = "cleanora.com"
